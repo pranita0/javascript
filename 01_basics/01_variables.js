57ceca3c6cf3d67f36ef8890291efbb4dc0c0837
pranita0 <168665723+pranita0@users.noreply.github.com>
2024-06-05 15:57:35 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 145554;
+let accountEmail = "juhi@gmail.com";
+var accountPassword = "12345";
+accountCity = "Nagpur";
+let accountState ;
+
+//accountId= 2// not allowed
+
+/*
+Prefer not to use var because of issue in block scope and functional scope.
+
+*/
+
+accountEmail = "hdcdjnc"
+accountPassword = " 23444"
+accountCity = "Hyd"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treats all js code as newer version
+
+// alert(3 + 3); //undefined we are using node js, not browser
+
+console.log(3 + 3); // code readability should be high
+
+console.log("juhi");
+
+let name ="juhi"
+
+let age=23
+ let isLoggedIn = true;
+ let state; //ex of undefined
+ let newstate = null; //ex of null type setb as zero
+
+ //Primitive Data types
+ //number- 2 to the power of 52/53
+ //bigInt- 
+ //string- ""
+ //boolean- true or false
+ //null is standalone value
+ //undefined- value is not assigned yet
+ // symbol- unique
+
+
+ //object
+ console.log(typeof age);
+ console.log(typeof null); //output is object
+ console.log(typeof undefined); //output is undefined
